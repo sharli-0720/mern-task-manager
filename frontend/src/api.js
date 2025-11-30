5463f57211987f49926c1c11cd7f4eabf96c875e
Sharli Iris John Baskar <sharlijb20@gmail.com>
2025-11-30 13:02:29 +0530

    Point frontend to new Render backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,8 @@
+// frontend/src/api.js
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://mern-backend-i4jgg.onrender.com/api",
-  withCredentials: true,
+  baseURL: "https://mern-task-manager-2-cl4y.onrender.com/api",
 });
 
 export default API;
